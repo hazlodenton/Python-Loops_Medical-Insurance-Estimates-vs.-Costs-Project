f5430c5a795704212b26bfb45f0f3836bf724e4a
hazlodenton <90017105+hazlodenton@users.noreply.github.com>
2023-03-12 15:43:25 +0000

    Add files via upload

---
+++ Python Loops_Medical Insurance Estimates vs. Costs Project.py
@@ -0,0 +1,28 @@
+#Python Loops: Medical Insurance Estimates vs. Costs Project
+names = ["Judith", "Abel", "Tyson", "Martha", "Beverley", "David", "Anabel"]
+estimated_insurance_costs = [1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, 7000.0]
+actual_insurance_costs = [1100.0, 2200.0, 3300.0, 4400.0, 5500.0, 6600.0, 7700.0]
+
+#Add your code here
+total_cost = 0
+for actual in actual_insurance_costs:
+  total_cost += actual
+print(total_cost)
+average_cost = total_cost / len(actual_insurance_costs)
+print("Average Insurance Cost: "+ str(average_cost) + " dollars")
+print("---------")
+
+for i in range(len(names)): 
+  name = names[i]
+  insurance_cost = actual_insurance_costs[i]
+  print("The insurance cost for " + str(name) + " is " + str(insurance_cost) + " dollars.")
+  if insurance_cost > average_cost:
+    print(str(name) + "'s" + " insurance cost is above the average")
+  elif insurance_cost < average_cost:
+    print(str(name) + "'s" + " insurance cost is below the average")
+  else: 
+    print(str(name) + "'s" + " insurance cost is equal to the average")
+  print("--------")
+
+updated_estimated_costs = [estimated_cost * 11/10 for estimated_cost in estimated_insurance_costs]
+print(updated_estimated_costs)
